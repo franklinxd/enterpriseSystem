9aea179b783bff8142fedda736bb82ab676c1233
Josue <Gr.321@email.com>
2018-11-06 16:02:02 -0600

    JpaUtil

--- src/java/com/enterprisesystem/beans/util/JPAUtil.java
+++ src/java/com/enterprisesystem/beans/util/JPAUtil.java
@@ -5,10 +5,24 @@
  */
 package com.enterprisesystem.beans.util;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author franklin.ramosusam
  */
 public class JPAUtil {
-    
+    private static final EntityManagerFactory emf;
+    static{
+        try {
+            emf=Persistence.createEntityManagerFactory("");
+        } catch (Exception ex) {
+            System.out.println("Creacion de sesion fallo"+ex);
+            throw new ExceptionInInitializerError(ex); 
+        }
+    }
+    public static EntityManagerFactory geEntityManagerFactory(){
+        return emf;
+    }
 }
