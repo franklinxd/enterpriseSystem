ce6cedbfa5782c44b7d1b69868ad395d20c20347
Unknown <mariela.garcia.raices.superate@gmail.com>
2018-11-13 09:42:18 -0600

    DETALLEDAO

--- src/java/com/enterprisesystem/dao/DetalleDAO.java
+++ src/java/com/enterprisesystem/dao/DetalleDAO.java
@@ -5,10 +5,42 @@
  */
 package com.enterprisesystem.dao;
 
-/**
- *
- * @author gerson.ruizusam
- */
+import com.enterprisesystem.modelo.DetallePlanilla;
+import com.enterprisesystem.modelo.Planilla;
+import com.enterprisesystem.util.JPAUtil;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+
+
 public class DetalleDAO {
-    
+   public List<DetallePlanilla> buscarTodo(){
+        EntityManagerFactory emf = JPAUtil.getJPAFactory();
+        EntityManager em= emf.createEntityManager();
+        TypedQuery consulta = em.createQuery("Select p from Planilla p",DetallePlanilla.class);
+        List<DetallePlanilla> listaDetalle = consulta.getResultList();
+        return listaDetalle;
+    }
+    public void borrar(DetallePlanilla detalle){
+        EntityManagerFactory emf =JPAUtil.getJPAFactory();
+        EntityManager em= emf.createEntityManager();
+        em.getTransaction().begin();
+        em.remove(em.merge(detalle));
+        em.getTransaction().commit();
+    }
+    public void insertar(DetallePlanilla detalle){
+        EntityManagerFactory emf =JPAUtil.getJPAFactory();
+        EntityManager em= emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(em.merge(detalle));
+        em.getTransaction().commit();
+    }
+    public void actualizar(DetallePlanilla detalle){
+        EntityManagerFactory emf =JPAUtil.getJPAFactory();
+        EntityManager em= emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(em.merge(detalle));
+        em.getTransaction().commit();
+    } 
 }
