9b0a5fba3ed5040a141ed0131c7548999548d0f7
ManuelS <manuel.suarez1c@gmail.com>
2018-11-22 14:32:07 -0600

    Nuevo Script

--- docs/enterprise.sql
+++ docs/enterprise.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `enterprisesystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `enterprisesystem`;
 -- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
 --
 -- Host: localhost    Database: enterprisesystem
@@ -245,4 +243,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-22 11:19:10
+-- Dump completed on 2018-11-22 14:29:53
