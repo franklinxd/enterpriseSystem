0ebe47064ddb2a29cfe0e9325dea437999e06551
Franklin Ramos <franklitorr@gmail.com>
2018-12-03 11:24:30 -0600

    JasperReport library

---
+++ src/java/com/enterprisesystem/util/Reporter.java
@@ -0,0 +1,14 @@
+package com.enterprisesystem.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Reporter {
+    
+    public void descargarPdf(){
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("userName", "Franklin");
+        
+    }
+}
